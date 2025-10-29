9a1ea04af9893fe5afcb21eb64a0281efe380aeb
Yossi Tyberg <tybergyossi@gmail.com>
2025-10-29 17:15:42 +0200

    stage 0 partnerA1

---
+++ Stage0/Program0144.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome0144();
+            Console.ReadKey();
+            WelcomeYYYY();
+            Console.ReadKey();
+        }
+
+        private static void WelcomeYYYY()
+        {
+            return;
+        }
+
+        private static void Welcome0144()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine() ?? "ERROR";
+            Console.WriteLine("{0}, welcome to my first console application", name);
+            Console.WriteLine("Press any key to continue");
+        }
+    }
+}
