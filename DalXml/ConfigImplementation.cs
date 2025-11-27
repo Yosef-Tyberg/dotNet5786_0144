d642825995ccdbc5ca42f17454bce11151b8a954
Yossi Tyberg <tybergyossi@gmail.com>
2025-11-27 02:09:56 +0200

    stage 3 steps 1-3 (preparing xml files)

---
+++ DalXml/ConfigImplementation.cs
@@ -0,0 +1,64 @@
+﻿using DalApi;
+
+namespace Dal;
+
+/// <summary>
+/// DAL implementation of <see cref="DalApi.IConfig"/> that forwards to the internal XML Config store.
+/// </summary>
+internal class ConfigImplementation : IConfig
+{
+    /// <inheritdoc />
+    public DateTime Clock
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+
+    /// <inheritdoc />
+    public int AdminId { get => Config.AdminId; set => Config.AdminId = value; }
+
+    /// <inheritdoc />
+    public string AdminPassword { get => Config.AdminPassword; set => Config.AdminPassword = value; }
+
+    /// <inheritdoc />
+    public double AvgCarSpeedKmh { get => Config.AvgCarSpeedKmh; set => Config.AvgCarSpeedKmh = value; }
+
+    /// <inheritdoc />
+    public double AvgMotorcycleSpeedKmh { get => Config.AvgMotorcycleSpeedKmh; set => Config.AvgMotorcycleSpeedKmh = value; }
+
+    /// <inheritdoc />
+    public double AvgBicycleSpeedKmh { get => Config.AvgBicycleSpeedKmh; set => Config.AvgBicycleSpeedKmh = value; }
+
+    /// <inheritdoc />
+    public double AvgWalkingSpeedKmh { get => Config.AvgWalkingSpeedKmh; set => Config.AvgWalkingSpeedKmh = value; }
+
+    /// <inheritdoc />
+    public double? MaxGeneralDeliveryDistanceKm { get => Config.MaxGeneralDeliveryDistanceKm; set => Config.MaxGeneralDeliveryDistanceKm = value; }
+
+    /// <inheritdoc />
+    public TimeSpan MaxDeliveryTimeSpan { get => Config.MaxDeliveryTimeSpan; set => Config.MaxDeliveryTimeSpan = value; }
+
+    /// <inheritdoc />
+    public TimeSpan RiskRange { get => Config.RiskRange; set => Config.RiskRange = value; }
+
+    /// <inheritdoc />
+    public TimeSpan InactivityRange { get => Config.InactivityRange; set => Config.InactivityRange = value; }
+
+    /// <inheritdoc />
+    public string? CompanyFullAddress { get => Config.CompanyFullAddress; set => Config.CompanyFullAddress = value; }
+
+    /// <inheritdoc />
+    public double? Latitude { get => Config.Latitude; set => Config.Latitude = value; }
+
+    /// <inheritdoc />
+    public double? Longitude { get => Config.Longitude; set => Config.Longitude = value; }
+
+
+    /// <summary>
+    /// Resets the underlying <see cref="Config"/> to its defaults.
+    /// </summary>
+    public void Reset()
+    {
+        Config.Reset();
+    }
+}
---
+++ DalXml/CourierImplementation.cs
@@ -0,0 +1,44 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class CourierImplementation : ICourier
+{
+    public void Create(Courier item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Courier? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Courier? Read(Func<Courier, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Courier?> ReadAll(Func<Courier, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Courier item)
+    {
+        throw new NotImplementedException();
+    }
+}
+
